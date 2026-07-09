c5e43bb1280e5719cbb88fa21002a687d3b9db01
ramzyameen10-arch <ramzyameen10@gmail.com>
2026-07-10 00:10:49 +0300

    Delete paddle.js

--- paddle.js
+++
@@ -1,2 +0,0 @@
-// Paddle.js v3 Stable Core
-if(!window.Paddle){!function(w,d,s,u,p){w[p]=w[p]||function(){(w[p].q=w[p].q||[]).push(arguments)},w[p].l=+new Date;var e=d.createElement(s);e.async=1,e.src=u;var n=d.getElementsByTagName(s)[0];n.parentNode.insertBefore(e,n)}(window,document,"script","https://assets.paddle.com/paddle/v3/paddle.js","Paddle")}
