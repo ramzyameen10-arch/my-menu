154b166d38e3ee9157b1e836e740865447477651
ramzyameen10-arch <ramzyameen10@gmail.com>
2026-07-09 23:09:14 +0300

    Create paddle.js

---
+++ paddle.js
@@ -0,0 +1,1 @@
+!function(w,d,s,u,p){w[p]=w[p]||function(){(w[p].q=w[p].q||[]).push(arguments)},w[p].l=+new Date;var e=d.createElement(s);e.async=1,e.src=u;var n=d.getElementsByTagName(s)[0];n.parentNode.insertBefore(e,n)}(window,document,"script","https://cdn.paddle.com/paddle/v3/paddle.js","Paddle");
